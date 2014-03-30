feb7a093fae94650054141130ef1be31b3a9437a
Mats Fredriksson <mats@mfpq.com>
2014-03-30 14:14:13 +0200

    update

--- NeuralNet/neuron.h
+++ NeuralNet/neuron.h
@@ -1,9 +1,15 @@
 #include <vector>
+#include "../utils.h"
 
 class Neuron
 {
 public:
-	Neuron(int numInputs);
+	Neuron(int numInputs)
+	: m_numInputs(numInputs)
+	{
+		for(int i = 0; i < m_numInputs + 1; ++i)
+			m_weights.push_back(randFloat(-1, 1));
+	}
 private:
 	int m_numInputs;
 	std::vector<double> m_weights;
--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "board.h"
 #include <iostream>
-
+#include "utils.h"
 
 int main()
 {
 	Board b(0);
+	for(int i = 0; i < 10; ++i)
+		std::cout << randInt(10) << std::endl;
 
-
+	for(int i = 0; i < 10; ++i)
+		std::cout << randFloat(1) << std::endl;
 }
